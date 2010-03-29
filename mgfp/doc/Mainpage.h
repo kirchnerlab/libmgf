e4678963e77792e84c8b5bff78f2150bce01cb52
Marc Kirchner <marc.kirchner@childrens.harvard.edu>
2010-03-29 02:32:17 +0000

    - some documentation updates - added a logo

--- mgfp/doc/Mainpage.h
+++ mgfp/doc/Mainpage.h
@@ -1,29 +1,42 @@
 /*!
 
+\image html mgfp_small.png 
+
 \mainpage mgfp: Mascot Generic Format (MGF) Parser
+
 \author Marc Kirchner <marc.kirchner@childrens.harvard.edu>
 
 \section sec_intro Introduction
-\c mgfp is a flex/bison-based C++ MGF parser library.
+\c mgfp is a flex/bison-based C++ MGF parser library.<br>
+
+The library is provided 
+under the terms of a BSD license and can thus be used in academic and 
+non-academic environments.
 
 \section sec_install Installation
 
 \subsection sec_install_bin Obtaining a Binary Package
 Binary packages for Microsoft Windows, Linux (64 bit, built on Ubuntu 10.4) and
-MacOS X (Snow Leopard) are available for download from the Steen Lab software
-page at http://software.steenlab.org/mgfp .
+MacOS X (Snow Leopard) are available for download from 
+
+\li the Steen Lab software page at http://software.steenlab.org/mgfp .
+
+Additionally, we are in the process of migrating MGFp to sourceforge.
+This will allow SVN access for anyone who wants to contribute.
+
+\li (Future) MGFp project page at sourceforge: http://mgfp.sf.net .
 
 \subsection sec_install_src Building from Source
 Building \c mgfp from source is straightforward. However, it requires a working
 CMake build system (available from http://cmake.org/) and CMake >= 2.6.
 
-With cmake available, the building process is
+With cmake available, the build process is
 \verbatim
  tar xvzf mgfp-xxxxxxx.tar.gz
- cd mgfp-xxxxxxx
- mkdir build
+ mkdir mgfp-build
+ cd mgfp-build
  cd build
- cmake ..
+ cmake ../mgfp-xxxxxxx
  make
  make test
  make install
@@ -31,7 +44,6 @@ With cmake available, the building process is
 
 Optionally, if you want to build your own precompiled packages, you can add
 \verbatim
-  cmake ..
   make package
 \endverbatim
 
