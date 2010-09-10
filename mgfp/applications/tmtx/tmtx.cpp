05ed80b9e8dfd8258df23ce870e48037dd31182c
Marc Kirchner <marc.kirchner@childrens.harvard.edu>
2010-09-10 15:30:49 +0000

    - bugfix and generalization to TMT and iTRAQ-4

--- mgfp/applications/tmtx/tmtx.cpp
+++ mgfp/applications/tmtx/tmtx.cpp
@@ -22,6 +22,8 @@
  */
 
 #include <cmath>
+#include <fstream>
+#include <iomanip>
 #include <iostream>
 #include <iterator>
 #include <tr1/array>
@@ -62,35 +64,73 @@ mgf::MgfSpectrum::iterator findClosestMz(mgf::MgfSpectrum::iterator begin,
     }
 }
    
-static const double tmtMasses[10] = 
-  { 124.0, 125.0, 126.0, 127.0, 128.0,
-    129.0, 130.0, 131.0, 132.0, 133.0 };
+enum LabelType { TMT, ITRAQ4, SIZE };
+static const size_t sizes[SIZE] = { 10, 8 };
+static const double masses[SIZE][10] = 
+  {
+    { 124.0, 125.0, 126.0, 127.0, 128.0,
+      129.0, 130.0, 131.0, 132.0, 133.0 },
+    { 112.1, 113.1,  114.1112, 115.1083, 116.1116,
+      117.115, 118.1, 119.1, 0.0, 0.0 }};
 
 int main(int argc, char *argv[])
 {
     namespace po = boost::program_options;
     po::options_description desc("Allowed options");
     bool verbose = false;
+    std::string inputfile;
+    size_t precision;
     desc.add_options()
         ("help,h", "produce help message")
         ("verbose,v", po::value(&verbose)->zero_tokens(), 
          "verbose output (good to pinpoint parsing errors)")
+        ("type,t", po::value<std::string>(), "label type: one of TMT, ITRAQ4")
+        ("input,i", po::value<std::string>(&inputfile)->default_value("-"),
+         "input filename, defaults to stdin")
+        ("tee,e", po::value<std::string>(), "run in tee mode: extract label "
+          "abundances to specified file and simply pipe MGF data from stdin "
+          "to stdout.")
+        ("precision,p", po::value<size_t>(&precision)->default_value(6),
+         "number of significant digits in output")
     ;
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
     po::notify(vm);    
     // check if the user asked for help
-    if (vm.count("help")) {
+    if (vm.count("help") || !vm.count("type")) {
         std::cerr << desc;
         return 1;
+    }
+    // figure put which labeling technique we use
+    LabelType labelType;
+    if (vm["type"].as<std::string>() == "TMT") {
+        labelType = TMT;
+    } else if (vm["type"].as<std::string>() == "ITRAQ4") {
+        labelType = ITRAQ4;
+    } else {
+        std::cerr << vm["type"].as<std::string>() << std::endl;
+        std::cerr << desc;
+        return 1;
+    }
+    // initialize the masses
+    std::vector<double> labelMasses(sizes[labelType]);
+    for (size_t n = 0; n < sizes[labelType]; ++n) {
+        labelMasses[n] = masses[labelType][n];
     }
     // prepare the parser
     mgf::MgfFile m;
     mgf::Driver driver(m);
     driver.trace_parsing = verbose;
     driver.trace_scanning = verbose;
-    // parse input 
-    bool result = driver.parse_stream(std::cin);
+    // parse input
+    bool result;
+    if (inputfile == "-") {
+        result = driver.parse_stream(std::cin);
+    } else {
+        std::ifstream ifs(inputfile.c_str());
+        result = driver.parse_stream(ifs);
+        ifs.close();
+    } 
     if (!result) {
         std::cerr << std::endl 
           << "Error parsing data stream (use -v for details)." << std::endl;
@@ -98,27 +138,33 @@ int main(int argc, char *argv[])
     }
     typedef mgf::MgfFile::iterator MFI;
     // iterate over all spectra
+    std::vector<double> obsAbundances(sizes[labelType]);
     for (MFI i = m.begin(); i != m.end(); ++i) {
         // sort
         std::sort(i->begin(), i->end(), mgf::LessThanMz()); 
         typedef mgf::MgfSpectrum::iterator MSI;
         // extract TMT reproter ion intensities
-        double obsTmtAbundances[6] = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
-        for (size_t n = 0; n < 6; ++n) {
-            MSI closestIt = findClosestMz(i->begin(), i->end(), tmtMasses[n]);
-            // check if there is a MS2 centroid 
-            if (std::abs(closestIt->first - tmtMasses[n]) < 0.4) {
-                obsTmtAbundances[n] = closestIt->second;
-            } else {
-                obsTmtAbundances[n] = 0.0;
+        bool atLeastOneEntry = false;
+        for (size_t n = 0; n < sizes[labelType]; ++n) {
+            MSI closestIt = findClosestMz(i->begin(), i->end(), labelMasses[n]);
+            if (closestIt != i->end()) {
+                // check if there is an MS2 centroid 
+                if (std::abs(closestIt->first - labelMasses[n]) < 0.4) {
+                    obsAbundances[n] = closestIt->second;
+                    if (!atLeastOneEntry) atLeastOneEntry = true;
+                } else {
+                    obsAbundances[n] = 0.0;
+                }
             }
         }
         // extract into file
-        std::cout << i->getTITLE();
-        for (size_t n = 0; n < 6; ++n) {
-            std::cout << '\t' << obsTmtAbundances[n];
+        if (atLeastOneEntry) {
+            std::cout << i->getTITLE();
+            for (size_t n = 0; n < sizes[labelType]; ++n) {
+                std::cout << '\t' << std::setprecision(precision) << obsAbundances[n];
+            }
+            std::cout << '\n'; // don't use std::endl, save the std::flush.
         }
-        std::cout << std::endl;
     }
     return 0;
 }
