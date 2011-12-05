2504f1b7727c49265b28d6d8516d59db12b3dbd8
David Sichau <mail@sichau.eu>
2011-12-05 15:26:47 -0500

    working version

--- applications/ms2preproc_libpipe/ms2preproc_libpipe.cpp
+++ applications/ms2preproc_libpipe/ms2preproc_libpipe.cpp
@@ -36,9 +36,224 @@
 #include "Algorithms/TopXAlgorithm.hpp"
 #include "Algorithms/TopXInWindowsOfSizeZAlgorithm.hpp"
 #include "Algorithms/TopXInYRegionsAlgorithm.hpp"
-#include "Algorithms/MGFFilePrinterMerge.hpp"
-#include "Algorithms/MgfFileSpliter.hpp"
 
+
+namespace Ms2Preproc {
+
+
+
+class MgfFileSpliterFour : public libpipe::rtc::Algorithm
+{
+
+    public:
+        static libpipe::rtc::Algorithm* create()
+        {
+            return new MgfFileSpliterFour;
+        }
+
+        virtual ~MgfFileSpliterFour()
+        {
+        }
+
+        void update(libpipe::Request& req)
+        {
+
+            boost::shared_ptr<libpipe::rtc::SharedData<mgf::MgfFile> > mgfInputFile =
+                    boost::dynamic_pointer_cast<
+                            libpipe::rtc::SharedData<mgf::MgfFile> >(
+                        this->getPort("MGFInputFile"));
+
+            boost::shared_ptr<libpipe::rtc::SharedData<mgf::MgfFile> > mgfOutputFile1 =
+                    boost::dynamic_pointer_cast<
+                            libpipe::rtc::SharedData<mgf::MgfFile> >(
+                        this->getPort("MGFOutputFile1"));
+            boost::shared_ptr<libpipe::rtc::SharedData<mgf::MgfFile> > mgfOutputFile2 =
+                    boost::dynamic_pointer_cast<
+                            libpipe::rtc::SharedData<mgf::MgfFile> >(
+                        this->getPort("MGFOutputFile2"));
+            boost::shared_ptr<libpipe::rtc::SharedData<mgf::MgfFile> > mgfOutputFile3 =
+                    boost::dynamic_pointer_cast<
+                            libpipe::rtc::SharedData<mgf::MgfFile> >(
+                        this->getPort("MGFOutputFile3"));
+            boost::shared_ptr<libpipe::rtc::SharedData<mgf::MgfFile> > mgfOutputFile4 =
+                    boost::dynamic_pointer_cast<
+                            libpipe::rtc::SharedData<mgf::MgfFile> >(
+                        this->getPort("MGFOutputFile4"));
+
+
+            mgfInputFile->lock();
+            mgfOutputFile1->lock();
+            mgfOutputFile2->lock();
+            mgfOutputFile3->lock();
+            mgfOutputFile4->lock();
+
+            if (mgfOutputFile1->get()->size() > 0) {
+                LIBPIPE_PIPELINE_TRACE(req, "MGF File already splited");
+
+            } else {
+                LIBPIPE_PIPELINE_TRACE(req, "Starting spliting MGF File");
+
+                int div = mgfInputFile->get()->size() / 4;
+
+                mgf::MgfFile* temp1 = new mgf::MgfFile;
+                mgf::MgfFile* temp2 = new mgf::MgfFile;
+                mgf::MgfFile* temp3 = new mgf::MgfFile;
+                mgf::MgfFile* temp4 = new mgf::MgfFile;
+
+                mgf::MgfFile::iterator it = mgfInputFile->get()->begin();
+
+                temp1->assign(it, it + div);
+                it = it + div;
+                temp2->assign(it, it + div);
+                it = it + div;
+                temp3->assign(it, it + div);
+                it = it + div;
+                temp4->assign(it, mgfInputFile->get()->end());
+
+                mgfOutputFile1->set(temp1);
+                mgfOutputFile2->set(temp2);
+                mgfOutputFile3->set(temp3);
+                mgfOutputFile4->set(temp4);
+                LIBPIPE_PIPELINE_TRACE(req, "MGF File successful splited.");
+
+            }
+
+            mgfOutputFile1->unlock();
+            mgfOutputFile2->unlock();
+            mgfOutputFile3->unlock();
+            mgfOutputFile4->unlock();
+            mgfInputFile->unlock();
+
+        }
+
+    protected:
+
+    private:
+        MgfFileSpliterFour() :
+                libpipe::rtc::Algorithm()
+        {
+            ports_["MGFInputFile"] = boost::make_shared<
+                    libpipe::rtc::SharedData<mgf::MgfFile> >();
+            ports_["MGFOutputFile1"] = boost::make_shared<
+                    libpipe::rtc::SharedData<mgf::MgfFile> >(new mgf::MgfFile);
+            ports_["MGFOutputFile2"] = boost::make_shared<
+                    libpipe::rtc::SharedData<mgf::MgfFile> >(new mgf::MgfFile);
+            ports_["MGFOutputFile3"] = boost::make_shared<
+                    libpipe::rtc::SharedData<mgf::MgfFile> >(new mgf::MgfFile);
+            ports_["MGFOutputFile4"] = boost::make_shared<
+                    libpipe::rtc::SharedData<mgf::MgfFile> >(new mgf::MgfFile);
+        }
+
+        static const bool registerLoader()
+        {
+            std::string ids = "MgfFileSpliterFour";
+            return libpipe::rtc::AlgorithmFactory::instance().registerType(ids,
+                MgfFileSpliterFour::create);
+        }
+        /// true is class is registered in Algorithm Factory
+        static const bool registered_;
+};
+const bool MgfFileSpliterFour::registered_ = registerLoader();
+
+
+class MgfFileMergerPrinter : public libpipe::rtc::Algorithm
+{
+
+    public:
+        static libpipe::rtc::Algorithm* create()
+        {
+            return new MgfFileMergerPrinter;
+        }
+
+        virtual ~MgfFileMergerPrinter()
+        {
+        }
+
+        void update(libpipe::Request& req)
+        {
+
+            boost::shared_ptr<libpipe::rtc::SharedData<mgf::MgfFile> > mgfOutputFile1 =
+                    boost::dynamic_pointer_cast<
+                            libpipe::rtc::SharedData<mgf::MgfFile> >(
+                        this->getPort("MGFOutputFile1"));
+            boost::shared_ptr<libpipe::rtc::SharedData<mgf::MgfFile> > mgfOutputFile2 =
+                    boost::dynamic_pointer_cast<
+                            libpipe::rtc::SharedData<mgf::MgfFile> >(
+                        this->getPort("MGFOutputFile2"));
+            boost::shared_ptr<libpipe::rtc::SharedData<mgf::MgfFile> > mgfOutputFile3 =
+                    boost::dynamic_pointer_cast<
+                            libpipe::rtc::SharedData<mgf::MgfFile> >(
+                        this->getPort("MGFOutputFile3"));
+            boost::shared_ptr<libpipe::rtc::SharedData<mgf::MgfFile> > mgfOutputFile4 =
+                    boost::dynamic_pointer_cast<
+                            libpipe::rtc::SharedData<mgf::MgfFile> >(
+                        this->getPort("MGFOutputFile4"));
+
+            LIBPIPE_PIPELINE_TRACE(req, "Starting writting MGF File");
+
+            std::string outfilename = parameters_.get<std::string>("outfile");
+            std::ofstream out(outfilename.c_str());
+            if (!out.good()) {
+                libpipe_fail("Error in generating output file");
+            }
+            out
+                    << "# MGF created using ms2preproc, (c) 2009 Marc Kirchner.\n"
+                    << "# This program accompanies Renard BY, Kirchner M, Monigatti F, Invanov AR,\n"
+                    << "# Rappsilber J, Winter D, Steen JAJ, Hamprecht FA, Steen H, When Less\n"
+                    << "# Can Yield More - Computational Preprocessing of MS/MS Spectra for\n"
+                    << "# Peptide Identification, Proteomics (2009).\n";
+
+            out << std::endl;
+
+            out.setf(std::ios_base::fixed, std::ios_base::floatfield);
+            out.precision(parameters_.get<unsigned int>("precision"));
+
+            mgfOutputFile1->lock();
+            mgfOutputFile2->lock();
+            mgfOutputFile3->lock();
+            mgfOutputFile4->lock();
+
+            out << *mgfOutputFile1->get() << std::endl;
+            out << *mgfOutputFile2->get() << std::endl;
+            out << *mgfOutputFile3->get() << std::endl;
+            out << *mgfOutputFile4->get() << std::endl;
+
+            mgfOutputFile1->unlock();
+            mgfOutputFile2->unlock();
+            mgfOutputFile3->unlock();
+            mgfOutputFile4->unlock();
+
+            LIBPIPE_PIPELINE_TRACE(req, "MGF File successful written.");
+        }
+
+    protected:
+
+    private:
+        MgfFileMergerPrinter() :
+                libpipe::rtc::Algorithm()
+        {
+            ports_["MGFOutputFile1"] = boost::make_shared<
+                    libpipe::rtc::SharedData<mgf::MgfFile> >();
+            ports_["MGFOutputFile2"] = boost::make_shared<
+                    libpipe::rtc::SharedData<mgf::MgfFile> >();
+            ports_["MGFOutputFile3"] = boost::make_shared<
+                    libpipe::rtc::SharedData<mgf::MgfFile> >();
+            ports_["MGFOutputFile4"] = boost::make_shared<
+                    libpipe::rtc::SharedData<mgf::MgfFile> >();
+        }
+
+        static const bool registerLoader()
+        {
+            std::string ids = "MgfFileMergerPrinter";
+            return libpipe::rtc::AlgorithmFactory::instance().registerType(ids,
+                MgfFileMergerPrinter::create);
+        }
+        /// true is class is registered in Algorithm Factory
+        static const bool registered_;
+};
+const bool MgfFileMergerPrinter::registered_ = registerLoader();
+
+}
 
 namespace po = boost::program_options;
 
